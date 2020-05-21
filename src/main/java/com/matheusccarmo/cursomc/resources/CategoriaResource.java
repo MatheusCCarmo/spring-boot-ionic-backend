32b0cca5c11ceffcba41f4529562295b0fecb4dc
matheusccarmo <matheuscarvalho@windowslive.com>
2020-05-20 23:05:26 -0300

    REST testado

---
+++ src/main/java/com/matheusccarmo/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.matheusccarmo.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "REST está funcionando";
+	}
+}
