a84c17b2c451abdeae5105fbef0583f88000fda3
matheusccarmo <matheuscarvalho@windowslive.com>
2020-05-20 23:28:15 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/matheusccarmo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheusccarmo/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.matheusccarmo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusccarmo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(path = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCat = new ArrayList<Categoria>();
+		
+		listaCat.add(cat1);
+		listaCat.add(cat2);
+		return listaCat;
 	}
 }
